8141e031895a54e83b8c8b6ee7fd54340129159d
Raphael Peters <rappet@rappet.de>
2020-12-11 16:06:52 +0100

    examples: Added 'lines_lowercase' example

---
+++ examples/lines_lowercase.rs
@@ -0,0 +1,12 @@
+use std::io::{stdin, BufRead};
+use umlauts::UmlautsOwned;
+
+fn main() -> std::io::Result<()> {
+    for line in stdin().lock().lines() {
+        let mut line = line?.into_bytes();
+        line.make_utf8_umlauts_lowercase();
+        println!("{}", String::from_utf8(line).unwrap());
+    }
+
+    Ok(())
+}
